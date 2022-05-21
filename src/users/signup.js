ca750f8d922fd9cc29063785eed77a509c811d23
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-05-21 11:56:07 -0400

    code cleanup

--- src/users/signup.js
+++ src/users/signup.js
@@ -27,36 +27,32 @@ function SignUpForm() {
     <main>
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-sm-6 form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              required
-              value={user.username}
-              onChange={(e) => setUser({ ...user, username: e.target.value })}
-              className="form-control"
-              id="username"
-              name="username"
-            />
-          </div>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            required
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+            className="form-control"
+            id="username"
+            name="username"
+          />
         </div>
 
-        <div className="row">
-          <div className="col-sm-6 form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              required
-              value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              className="form-control"
-              id="password"
-              name="password"
-            />
-          </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            required
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            className="form-control"
+            id="password"
+            name="password"
+          />
         </div>
 
-        <input className="btn btn-primary" type="submit" value="Sign Up" />
+        <input type="submit" value="Sign Up" />
       </form>
     </main>
   );

af0332f592a38949360414208735aee061868aa9
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-05-23 19:24:06 -0400

    fix attempt

--- users/login.js
+++ users/login.js
@@ -1,27 +1,28 @@
+function login(e) {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
 
-async function handleSubmit(e) {
-  let username = document.getElementById('username').value
-  let password = document.getElementById('password').value
+  console.log(username);
 
-  console.log(username)
+  handleSubmit(e);
 
-  const response = await fetch(`http://localhost:5000/authentication/`, {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(credentials),
-  });
+  async function handleSubmit(e) {
+    const response = await fetch(`http://localhost:5000/authentication/`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  if (response.status === 200) {
-    setCurrentUser(data.user);
-    history.push(`/`);
-  } else {
-    setErrorMessage(data.message);
+    if (response.status === 200) {
+      setCurrentUser(data.user);
+      history.push(`/`);
+    } else {
+      setErrorMessage(data.message);
+    }
   }
-
-  return false
 }
